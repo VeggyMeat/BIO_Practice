f8f007b27c225f34cf0696f6efbac353d8080bc4
VeggyMeat <MacraeA@stpaulsschool.org.uk>
2022-12-26 15:36:49 +0000

    Speed it up a little bit

--- BIO_2001/Q3.py
+++ BIO_2001/Q3.py
@@ -41,27 +41,10 @@ def recursion(n, pack_here, left, there):
                     searched[key] = results[-1] - n - max(person1, person2)
 
     else:
-        for person in there:
-            new_left = left[:]
-            new_left.append(person)
-            new_left.sort()
-
-            new_there = there[:]
-            new_there.remove(person)
-            new_there.sort()
-            key = (True, tuple(new_left), tuple(new_there))
-            if key in searched:
-                amount = searched[key] + n + person
-                results.append(amount)
-                if amount < best:
-                    best = amount
-            else:
-                results.append(recursion(n + person, True, new_left, new_there))
-                searched[key] = results[-1] - n - person
-        # smallest = min(there)
-        # there.remove(smallest)
-        # left.append(smallest)
-        # return recursion(n + smallest, True, left, there)
+        smallest = min(there)
+        there.remove(smallest)
+        left.append(smallest)
+        return recursion(n + smallest, True, left, there)
 
     return min(results)
 
