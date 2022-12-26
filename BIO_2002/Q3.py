777c8f4fa9fd8baf70d8aa5f37395efa7d2887d3
VeggyMeat <MacraeA@stpaulsschool.org.uk>
2022-12-26 17:18:25 +0000

    Fixed a hole in Q3 that failed 1 testcase

--- BIO_2002/Q3.py
+++ BIO_2002/Q3.py
@@ -4,25 +4,22 @@ num = int(input())
 
 def facts(a):
     l = a ** 0.5
+    fact = []
     for b in range(2, a + 1):
         if b > l:
             break
 
         if a % b == 0:
-            return b, a // b
+            fact.append((b, a // b))
 
-    return None, None
+    return fact
 
 
 for x in range(7, num + 1):
-    fact1, fact2 = facts(x)
-    if fact1 is None:
+    factors = facts(x)
+    if not factors:
         dic.append(dic[-1] + 1)
     else:
-        value = dic[fact1] + dic[fact2]
-        for y in range(1, 5):
-            if dic[-y] + y < value:
-                value = dic[-y] + y
-        dic.append(value)
+        dic.append(min(min([dic[pair[0]] + dic[pair[1]] for pair in factors]), dic[-1] + 1))
 
 print(dic[-1])
